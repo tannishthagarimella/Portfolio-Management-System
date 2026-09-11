c09ce5da7071f6a207995ebcb63b758f785ec05d
tannishthagarimella <tannishthagarimella9@gmail.com>
2026-09-11 16:58:26 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,21 @@
+package com.portfolio.app.model;
+
+public class Student1{ 
+
+public Student1() {
+    System.out.println("Hi welcome to my class");
+}
+
+
+public Student1(int x) {
+    System.out.println(x);
+}
+
+
+public static void main(String[] args) {
+
+    Student1 s = new Student1();
+    Student1 s1 = new Student1(10);
+}
+     
+   }
